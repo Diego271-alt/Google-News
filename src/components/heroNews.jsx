740ea27c19f2bc5ee1633248de1f335492de0cf1
Diego Romero <dr87152@gmail.com>
2021-05-09 14:36:29 -0500

    new api

--- src/components/heroNews.jsx
+++ src/components/heroNews.jsx
@@ -7,20 +7,18 @@ export default function HeroNews(props) {
   useEffect(() => {
     setLoadData(true);
     fetch(
-      `https://free-news.p.rapidapi.com/v1/search?q=Technology&lang=en`,
+      `https://gnews.io/api/v4/search?q=Technology&token=325e4e442fe032567453dc05e3e4fe38`,
       {
         method: "GET",
         headers: {
-          "x-rapidapi-key":
-            "ca4e8559bfmshb36feb647ab99e4p1b37e4jsn3108e78e485c",
-          "x-rapidapi-host": "free-news.p.rapidapi.com",
+          "Cookie": "PHPSESSID=81lqsre1329k9988s4sl6f7a0a",
         },
         redirect: "follow",
       }
     )
       .then((response) => response.json())
       .then((data) => {
-        setArticle(data.articles[6]);
+        setArticle(data.articles[2]);
         setLoadData(false);
       });
   }, []);
@@ -46,7 +44,7 @@ export default function HeroNews(props) {
           <h2>{article.title}</h2>
         </div>
       </div>
-      <img src={article.media || "https://images.unsplash.com/photo-1426900985728-92d56f56fdb2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjIwNTI1NDA1&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit"} className="img-fluid" alt="image_article" />
+      <img src={article.image || "https://images.unsplash.com/photo-1426900985728-92d56f56fdb2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjIwNTI1NDA1&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit"} className="img-fluid" alt="image_article" />
     </div>
   );
 }
